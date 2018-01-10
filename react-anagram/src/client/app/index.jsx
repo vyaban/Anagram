f9ef1f3dfc9dce9b32b4a5b29a4422eb1ff5a9a1
Vikram Yabannavar <vyaban@gmail.com>
2018-01-10 13:17:36 -0800

    Add functionality to change color of original input when anagram letters are entered

--- react-anagram/src/client/app/index.jsx
+++ react-anagram/src/client/app/index.jsx
@@ -1,6 +1,88 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+class Letter extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            filled: this.props.filled ? "filled" : "notFilled",
+            letter: this.props.letter
+        }
+    }
+    
+    componentWillReceiveProps(nextProps) {
+      this.setState({
+            filled: nextProps.filled ? "filled" : "notFilled",
+            letter: nextProps.letter
+        });  
+    }
+    
+    render() {
+        return <span className={this.state.filled}>{this.state.letter}</span>
+    }
+}
+
+//processes what needs to be filled in or not
+class AnagramProgress extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            anagramCharCounts: this.props.anagramCharCounts,
+            origCharArr: this.props.origCharArr
+        };
+        
+        this.toSpanReducer = this.toSpanReducer.bind(this);
+        this.processChars = this.processChars.bind(this);
+    }
+    
+    componentWillReceiveProps(nextProps) {
+      this.setState(nextProps);  
+    }
+    
+    toSpanReducer(context, letter) {
+        console.log("Reduction Step");
+        console.log(context);
+        console.log(letter);
+        let charCounts = context.charCounts;
+        
+        if (charCounts[letter.toUpperCase()] == null || charCounts[letter.toUpperCase()] == 0) {
+            return {
+                spans: context.spans.concat(<Letter filled={false} letter={letter} />),
+                charCounts: charCounts
+            }
+        } else {
+            console.log(charCounts[letter.toUpperCase()]);
+            charCounts[letter.toUpperCase()] = charCounts[letter.toUpperCase()] - 1;
+            console.log(charCounts[letter.toUpperCase()]);
+                
+            return {
+                spans: context.spans.concat(<Letter filled={true} letter={letter} />),
+                charCounts: charCounts
+            }
+        }
+    }
+    
+    processChars() {
+        let context = {
+            spans: [], 
+            charCounts: this.state.anagramCharCounts
+        }
+        
+        //convert char array into span array
+        return this.state.origCharArr.reduce(this.toSpanReducer, context).spans;
+    }
+    
+    render() {
+        let output = this.processChars();
+        
+        return <div>
+            {output}
+        </div>
+    }
+}
+
 class Anagram extends React.Component {
     constructor(props) {
         super(props);
@@ -28,7 +110,6 @@ class Anagram extends React.Component {
                updatedBank[word.charAt(i)] = updatedBank[word.charAt(i)] + 1;
            }
         }
-        console.log(updatedBank);
         return updatedBank;
     }
     
@@ -48,7 +129,7 @@ class Anagram extends React.Component {
    render () {
      return <div>
              <input name="origWord" type="text" value={this.state.origWord.word} onChange={this.inputTextChange} />
-             <p>{this.state.origWord.word}</p>
+             <AnagramProgress anagramCharCounts={this.state.anagramWord.letters} origCharArr={this.state.origWord.word.split('')} />
              <input name="anagramWord" type="text" value={this.state.anagramWord.word} onChange={this.inputTextChange} />
          </div>;
    }
