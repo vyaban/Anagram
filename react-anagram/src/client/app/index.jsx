79258aeaeefdc372e8a73ac9a58d4537e2be222c
Vikram Yabannavar <vyaban@gmail.com>
2018-01-10 12:12:59 -0800

    Add two input fields and functionality to update their character counts when changed

--- react-anagram/src/client/app/index.jsx
+++ react-anagram/src/client/app/index.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!!</p>;
-  }
+class Anagram extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            origWord: {
+                word: props.word,
+                letters: {}
+            },
+            anagramWord: {
+                word: "",
+                letters: {}
+            }
+        };
+        
+        this.inputTextChange = this.inputTextChange.bind(this);
+        this.calculateCharacterBank = this.calculateCharacterBank.bind(this);
+    }
+    
+    calculateCharacterBank(word) {
+        let updatedBank = {};
+        for(let i = 0; i < word.length; i++) {
+           if (updatedBank[word.charAt(i)] == null) {
+               updatedBank[word.charAt(i)] = 1;
+           } else {
+               updatedBank[word.charAt(i)] = updatedBank[word.charAt(i)] + 1;
+           }
+        }
+        console.log(updatedBank);
+        return updatedBank;
+    }
+    
+    inputTextChange(event) {
+        let change = {};
+        const words = event.target.value;
+        const letters = this.calculateCharacterBank(words.replace(/\W/g, '').toUpperCase());
+        
+        change[event.target.name] = {
+            word: words,
+            letters: letters
+        };
+        this.setState(change);
+        
+    }
+    
+   render () {
+     return <div>
+             <input name="origWord" type="text" value={this.state.origWord.word} onChange={this.inputTextChange} />
+             <p>{this.state.origWord.word}</p>
+             <input name="anagramWord" type="text" value={this.state.anagramWord.word} onChange={this.inputTextChange} />
+         </div>;
+   }
 }
 
-render(<App/>, document.getElementById('app'));
+class App extends React.Component {
+    render () {
+        return <Anagram word="Type an anagram" />
+    }
+}
+
+render(<App />, document.getElementById('app'));
